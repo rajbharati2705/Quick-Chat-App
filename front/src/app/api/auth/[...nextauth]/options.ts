3e010b0cc7ec5b580b7a8639e16065f8c26b8ab9
Raj bharati <rajbharati@dsmartsys.in>
2025-02-15 14:49:46 -0600

    feat: Add next auth with google

---
+++ front/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,20 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+export const authOption: AuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  providers:[
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          }
+      })
+  ]
+};
